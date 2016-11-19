3c40f058ec3c9338f8f055ee43800d3034c08cb8
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-11-19 19:31:56 +0100

    Add 404 pge

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', '404 Page')
+
+@section('content')
+    <div class="page-for">
+        <h1 class="text-center">404</h1>
+        <h3 class="font-bold text-center">Page Not Found</h3>
+
+        <div class="error-desc text-center">
+            Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the
+            refresh button on your browser or try found something else in our app.
+        </div>
+    </div>
+@endsection
